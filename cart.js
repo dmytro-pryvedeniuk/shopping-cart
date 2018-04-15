1f13e37b4d7a1b802f39cfe28a74fc27e7c576fb
Dmytro Pryvedeniuk <dmytro.pryvedeniuk@gmail.com>
2018-04-15 14:12:26 +0300

    Finished validation

--- cart.js
+++ cart.js
@@ -1,120 +1,154 @@
-const products = [{
-    name: "Rock&roll",
-    image: "item1.jpg",
-    price: "12.50"
-}, {
-    name: "Pop",
-    image: "item1.jpg",
-    price: "9.50"
-}, {
-    name: "Hard rock",
-    image: "item1.jpg",
-    price: "78.2"
-}]
+(function() {
 
-const shippingAddress = {}
+    var products = [{
+        name: "Rock&roll",
+        image: "item1.jpg",
+        price: "12.50"
+    }, {
+        name: "Pop",
+        image: "item1.jpg",
+        price: "9.50"
+    }, {
+        name: "Hard rock",
+        image: "item1.jpg",
+        price: "78.2"
+    }];
 
-let orderItems = []
+    var state = {
+        orderItems: []
+    };
 
-function onload() {
-    for (var i in products) {
-        $(`<div class="col col-xs-12 col-md-6 col-lg-4"> 
-                <div> 
-                    <img src="Products/${products[i].image}"> 
-                </div> 
-                <button data-id="${i}" class="btn btn-default addToCart">Add to Cart</button>
-                <h2 class="name">${products[i].name}</h2> 
-                <p class="price">$${products[i].price}</p>
-            </div>`).appendTo($("#products"));
+    var app = {
+        initialize: function() {
+            this.setupListeners();
+        },
+
+        setupListeners: function() {
+            $(document).ready(app.onload);
+            $("#shipping-address-show").click(app.showShipTo);
+            $("#shipping-address-apply").click(app.applyShipTo);
+            $("#send").click(app.send);
+        },
+
+        send: function(e) {
+            let text = $("#shipToForm").serialize();
+            alert("The order has been sent: " + text);
+            $('#cartBody')
+                .empty()
+                .html('<p>The order has been sent. Thank you.</p>');
+            $(".addToCart").attr('disabled', '');
+        },
+
+        addToCart: function(e) {
+            const id = $(e.target).attr("data-id");
+
+            let item = state.orderItems.find(p=>p.id == id);
+            if (!item) {
+                item = {
+                    id: id,
+                    item: products[id],
+                    num: 0
+                };
+                state.orderItems.push(item);
+            }
+            item.num++;
+            app.refreshCart();
+        },
+
+        removeFromCart: function(e) {
+            const id = $(e.target).attr("data-id");
+            let item = state.orderItems.find(p=>p.id == id);
+            if (item)
+                item.num--;
+            state.orderItems = state.orderItems.filter(x=>x.num > 0);
+            app.refreshCart();
+        },
+
+        refreshCart: function() {
+            const cart = $("#cart");
+            cart.empty();
+
+            for (let i in state.orderItems) {
+                let item = state.orderItems[i].item
+                  , num = state.orderItems[i].num
+                  , id = state.orderItems[i].id;
+                cart.append(`<p>
+                    <button data-id="${id}" class="btn btn-danger btn-sm removeFromCart glyphicon glyphicon-minus"/>
+                    <button data-id="${id}" class="btn btn-primary btn-sm addFromCart glyphicon glyphicon-plus"/>
+                    ${num} x ${item.name} $${item.price}
+                </p>`);
+            }
+            $(".addFromCart").click(app.addToCart);
+            $(".removeFromCart").click(app.removeFromCart);
+
+            const totalReport = $("#totalreport");
+            totalReport.empty();
+            if (state.orderItems.length > 0) {
+                const subTotal = state.orderItems.map(x=>parseFloat(x.item.price) * x.num).reduce((p,c)=>p + c, 0);
+                const tax = subTotal * 0.1;
+                const total = subTotal + tax;
+                totalReport.append(`<p>Subtotal:$${subTotal.toFixed(2)}</p>
+                    <p>Tax:$${tax.toFixed(2)}</p>
+                    <p>Total:<b>$${total.toFixed(2)}</b></p>`);
+            }
+        },
+
+        onload: function() {
+            for (var i in products) {
+                $(`<div class="col col-xs-12 col-md-6 col-lg-4"> 
+                    <div> 
+                        <img src="Products/${products[i].image}"> 
+                    </div> 
+                    <button data-id="${i}" class="btn btn-default addToCart">Add to Cart</button>
+                    <h2 class="name">${products[i].name}</h2> 
+                    <p class="price">$${products[i].price}</p>
+                </div>`).appendTo($("#products"));
+            }
+            $(".addToCart").click(app.addToCart);
+        },
+
+        showShipTo: function() {
+            $("#myModal").modal("show");
+        },
+
+        validate: function() {
+            let formValid = true;
+
+            $('input').each((i,el)=>{
+                let formGroup = $(el).parents('.form-group');
+                let glyphicon = formGroup.find('.form-control-feedback');
+                if (el.checkValidity()) {
+                    formGroup.addClass("has-success").removeClass("has-error");
+                    glyphicon.addClass("glyphicon-ok").removeClass("glyphicon-remove");
+                } else {
+                    formGroup.addClass('has-error').removeClass('has-success');
+                    glyphicon.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+                    formValid = false;
+                }
+            }
+            );
+
+            if (formValid) {
+                $("#myModal").modal("hide");
+            }
+
+            return formValid;
+        },
+
+        applyShipTo: function() {
+            let validShipTo = app.validate();
+
+            if (!validShipTo) {
+                $("#shipping-address-show").removeClass("btn-success").addClass("btn-warning");
+                $("#shipping-address-icon").removeClass("glyphicon-ok").addClass("glyphicon-exclamation-sign");
+                $('#send').attr("disabled", true);
+                return;
+            }
+            $('#send').removeAttr('disabled');
+            $("#shipping-address-show").removeClass("btn-warning").addClass("btn-success");
+            $("#shipping-address-icon").removeClass("glyphicon-exclamation-sign").addClass("glyphicon-ok");
+        }
     }
 
-    $(".addToCart").click(addToCart);
-    $("#submitOrder").click(submitOrder);
-    $("#shipping-address-show").click(showShippingAddress);
-    $("#shipping-address-apply").click(applyShippingAddress);
-}
-
-function showShippingAddress()
-{
-    $("#myModal").modal("show");
-}
-
-function applyShippingAddress()
-{
-    $("#myModal").modal("hide");
-    
-    $("#shipping-address-show").removeClass("btn-warning").addClass("btn-success");
-    $("#shipping-address-status").removeClass("glyphicon-exclamation-sign")
-        .addClass("glyphicon-ok");
-    
-    shippingAddress.fullName = $("#fullname").val();
-    shippingAddress.zip = $("#zip").val();
-    shippingAddress.address1 = $("#address1").val();
-    shippingAddress.address2 = $("#address2").val();
-    shippingAddress.city = $("#city").val();
-    shippingAddress.state = $("#state").val();
-    shippingAddress.phone = $("#phone").val();
-}
-
-function addToCart(e) {
-    const id = $(e.target).attr("data-id");
-
-    let item = orderItems.find(p=>p.id == id);
-    if (!item) {
-        item = {
-            id: id,
-            item: products[id],
-            num: 0
-        };
-        orderItems.push(item);
-    }
-    item.num++;
-    refreshCart();
-}
-
-function removeFromCart(e) {
-    const id = $(e.target).attr("data-id");
-    let item = orderItems.find(p=>p.id == id);
-    if (item)
-        item.num--;
-    orderItems = orderItems.filter(x=>x.num > 0);
-    refreshCart();
-}
-
-function refreshCart() {
-    const cart = $("#cart");
-    cart.empty();
-
-    for (let i in orderItems) {
-        let item = orderItems[i].item;
-        let num = orderItems[i].num;
-        let id = orderItems[i].id;
-        cart.append(`<p>
-            <button data-id="${id}" class="btn btn-danger btn-sm removeFromCart glyphicon glyphicon-minus"/>
-            <button data-id="${id}" class="btn btn-primary btn-sm addFromCart glyphicon glyphicon-plus"/>
-            ${num} x ${item.name} $${item.price}
-            </p>`);
-    }
-    $(".addFromCart").click(addToCart);
-    $(".removeFromCart").click(removeFromCart);
-
-    const totalReport = $("#totalreport");
-    totalReport.empty();
-    if (orderItems.length > 0) {
-        const subTotal = orderItems.map(x=>parseFloat(x.item.price) * x.num).reduce((p,c)=>p + c, 0);
-        const tax = subTotal * 0.1;
-        const total = subTotal + tax;
-        totalReport.append(`<p>Subtotal:$${subTotal.toFixed(2)}</p>
-            <p>Tax:$${tax.toFixed(2)}</p>
-            <p>Total:<b>$${total.toFixed(2)}</b></p>`);
-    }
-    const itemsQty = orderItems.map(x=>x.num).reduce((p,c)=>p + c, 0);
-    $("#itemsqty").text(itemsQty);
-}
-
-
-function submitOrder(e) {
-    alert("The order has been sent.");
-}
-
-$(document).ready(onload);
+    app.initialize();
+}());
